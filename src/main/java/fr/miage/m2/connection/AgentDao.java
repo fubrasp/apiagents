1405751593cd0c6c9cba8323888bd6942fde76d9
idSaid <id.said@live.fr>
2017-12-20 15:59:20 +0100

    dao Impl

--- src/main/java/fr/miage/m2/connection/AgentDao.java
+++ src/main/java/fr/miage/m2/connection/AgentDao.java
@@ -2,6 +2,6 @@ package fr.miage.m2.connection;
 
 public interface AgentDao {
 
-    public void setAvailability(int possibleEvents);
+    public void setAvailability(int idAgent,int possibleEvents);
 
 }
--- src/main/java/fr/miage/m2/connection/AgentDaoImpl.java
+++ src/main/java/fr/miage/m2/connection/AgentDaoImpl.java
@@ -1,21 +1,23 @@
 package fr.miage.m2.connection;
 
-import fr.miage.m2.metier.Agent;
 import org.springframework.stereotype.Service;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Set;
-import java.util.TreeSet;
 
 @Service
 public class AgentDaoImpl implements AgentDao {
+
     @Override
-    public void setAvailability(int possibleEvents) {
+    public void setAvailability(int idAgent,int possibleEvents) {
 
         try {
+            PreparedStatement pstmt = PostgresConnection.getDbCon().conn.prepareStatement(
+                    "UPDATE agents_events SET capacite_events=? WHERE id_resource=?;"
+            );
 
+            pstmt.setInt(1, idAgent);
+            pstmt.setDouble(2, possibleEvents);
+            pstmt.executeUpdate();
         }catch (Exception e){
             e.printStackTrace();
         }
